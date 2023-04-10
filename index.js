f5d113771b4811897eb4351ef246651d840e4a40
dan-o <66097571+dpress13@users.noreply.github.com>
2023-04-10 13:12:17 -0400

    clean code in files

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-// Detecting Button Click
-
+// Detect which button / "letter" was clicked
 var numDrumButtons = document.querySelectorAll(".drum").length;
 for (var drumNum = 0; drumNum < numDrumButtons; drumNum++) {
   document.querySelectorAll(".drum")[drumNum].addEventListener("click", function() {
     var buttonInnerHTML = this.innerHTML;
     playSound(buttonInnerHTML);
-    buttonAnimation(buttonInnerHTML); // going to pass in the letter clicked to playSound with this method. saying send the inner html of the button that was clicked to the playSound method
-  }); //If a button was clicked, it's going to check the innerHTML of the button that was clicked and send that to make sound in order to play the relevant sound.
+    buttonAnimation(buttonInnerHTML);
+  });
 }
 
-// Detecting Keyboard Press
-
+// Detect which key / letter was pressed
 document.addEventListener("keydown", function(event) {
   var keyPressed = event.key;
-  keyPressed = keyPressed.toLowerCase(); // Allow capital letters to also activate animation and sounds
+  keyPressed = keyPressed.toLowerCase();
   playSound(keyPressed);
   buttonAnimation(keyPressed);
-  // If a keypress was detected instead, then the key's letter property that was pressed - "s" - will be sent to make sound
 });
 
 function playSound(key) {
-  switch (key) { //going to switch on key. will switch based on key parameter and play the relevant sound based on which key was passed in
+  switch (key) { 
 
     case "w":
       var tom1 = new Audio("sounds/tom-1.mp3");
-      tom1.play(); //is a method that will play the sound
+      tom1.play();
       break;
 
     case "a":
@@ -65,7 +62,7 @@ function playSound(key) {
 
 function buttonAnimation(currentKey) {
   var activeButton = document.querySelector("." + currentKey);
-  activeButton.classList.toggle("pressed");  //notice the semicolon here as if it were two older variables or js things we were adding under simpler functions
+  activeButton.classList.toggle("pressed");  
 
   setTimeout(function() {
     activeButton.classList.remove("pressed");
